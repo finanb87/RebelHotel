024d7f77d8af9814afbfa1ac85d2ad8d0e3fbd29
unknown <finan@.(none)>
2011-04-30 17:41:30 -0700

    unable to display yes/no for boolean in tables

--- src/main/java/edu/unlv/cs/rebelhotel/web/ApplicationConversionServiceFactoryBean.java
+++ src/main/java/edu/unlv/cs/rebelhotel/web/ApplicationConversionServiceFactoryBean.java
@@ -37,6 +37,17 @@ public class ApplicationConversionServiceFactoryBean extends FormattingConversio
 		};
 	}
 	
+	Converter<Boolean, String> getBooleanConverter() {
+		return new Converter<Boolean, String>() {
+			public String convert(Boolean param) {
+				if(param){
+					return "Yes";
+				}
+				return "No";
+			}
+		};
+	}
+	
 	Converter<edu.unlv.cs.rebelhotel.domain.Term, String> getTermConverter() {
 		return new Converter<edu.unlv.cs.rebelhotel.domain.Term, String>() {
 			public String convert(edu.unlv.cs.rebelhotel.domain.Term param) {
@@ -155,6 +166,7 @@ public class ApplicationConversionServiceFactoryBean extends FormattingConversio
 		// Register application converters and formatters
 		
 		registry.addConverter(getSetConverter());
+		registry.addConverter(getBooleanConverter());
 		registry.addConverter(getTermConverter());
 		registry.addConverter(getStudentConverter());
 		registry.addConverter(getSupervisorConverter());
